5e9ac46960ab9915541b089bfc53e3c67aa74825
griggheo <grig@gheorghiu.net>
2019-11-10 14:27:05 -0800

    change parent project

--- google-cloud-apis/test-google-translate.py
+++ google-cloud-apis/test-google-translate.py
@@ -1,5 +1,5 @@
 # pip install google-cloud-translate
-# export GOOGLE_APPLICATION_CREDENTIALS
+# export GOOGLE_APPLICATION_CREDENTIALS=~/.gcloud/python-workshops.json
 
 from google.cloud import translate
 client = translate.TranslationServiceClient()
@@ -8,5 +8,5 @@ print("Please enter text to be translated in English:")
 text_source = input()
 text = client.translate_text(contents=[text_source], 
     target_language_code='en', 
-    parent='projects/platinum-device-722')
+    parent='projects/python-workshops')
 print(text)
